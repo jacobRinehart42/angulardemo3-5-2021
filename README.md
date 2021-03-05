c5409ca125943bbd38e3e2b1e66aac54bcd4dee7
jacobRinehart42 <jacobofmirkwood@yahoo.com>
2021-03-05 13:13:40 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angulardemo3-5-2021
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angulardemo3-5-2021)
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Hello World";
+  imageURL =
+    "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=620&quality=85&auto=format&fit=max&s=56d5de4c5609ca98def0c3382bd569b4";
+}
