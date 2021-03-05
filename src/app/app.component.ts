b9ce2969468354b2ccca01322e490911525c6d91
jacobRinehart42 <jacobofmirkwood@yahoo.com>
2021-03-05 13:23:49 -0600

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,10 @@ export class AppComponent {
   name = "Hello World";
   imageURL =
     "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=620&quality=85&auto=format&fit=max&s=56d5de4c5609ca98def0c3382bd569b4";
+
+  message: string = "";
+  onClick(msg: string): void{
+    this.message = msg;
+  }
+
 }
